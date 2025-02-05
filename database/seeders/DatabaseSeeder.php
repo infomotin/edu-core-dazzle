3037fe45d6caf4c009abadd4e15baf0101143607
infomotin <motin.mmk.jr@gmail.com>
2025-02-05 09:32:56 +0600

    create admin system  chang model admin auth and guard dashbord and view

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,6 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call(AdminSeeder::class);
+        // $this->call(AdminSeeder::class);
     }
 }
